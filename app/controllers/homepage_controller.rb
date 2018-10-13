9f784bc4f749a69331e37026e09c845f1e4ed7c2
nafidurmus <nafidurmus07@gmail.com>
2018-10-13 14:08:31 +0300

    Added hurriyet api

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,4 +1,22 @@
 class HomepageController < ApplicationController
-  def index
-  end
+
+	def index
+		require 'uri'
+		require 'net/http'
+		require 'json'
+
+		url = URI("https://api.hurriyet.com.tr/v1/articles?$top=8")
+
+		http = Net::HTTP.new(url.host, url.port)
+		http.use_ssl = true
+		http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+		request = Net::HTTP::Get.new(url)
+		request["apikey"] = 'kendi apinizi yazınız'
+		request["accept"] = 'application/json'
+		
+
+		@response = http.request(request)
+		@hurriyet = JSON.parse(@response.body)
+  	end
 end
