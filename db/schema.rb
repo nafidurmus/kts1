e1f08c52903e0947d5f9001bc5f4e0c864b27f9a
nafidurmus <nafidurmus07@gmail.com>
2018-10-27 16:37:24 +0300

    some fix it & with pg

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2018_10_19_101514) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -31,8 +34,9 @@ ActiveRecord::Schema.define(version: 2018_10_19_101514) do
     t.float "weight"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "user_id"
+    t.bigint "user_id"
     t.index ["user_id"], name: "index_weights_on_user_id"
   end
 
+  add_foreign_key "weights", "users"
 end
